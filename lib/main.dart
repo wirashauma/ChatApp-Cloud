3251c0aeceee740f72ea83dae6e7fd9289d85e75
wirashauma <wirashaumaa@gmail.com>
2025-12-09 05:50:26 +0700

    Final: Menyelesaikan fitur Profile, Storage, dan perbaikan bug

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false,
-      // Halaman pertama yang dibuka adalah 'AuthCheckScreen'
+      // Halaman pertama adalah 'AuthCheckScreen'
       home: AuthCheckScreen(), 
     );
   }
